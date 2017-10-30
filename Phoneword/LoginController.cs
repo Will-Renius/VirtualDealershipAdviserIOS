89cafa137ed09a0de91b00d4c8e5cb1a84082fbb
GrenfellJ <jgrenfell30@gmail.com>
2017-10-30 19:01:09 -0400

    James: Bug fix for login segue

--- Phoneword/LoginController.cs
+++ Phoneword/LoginController.cs
@@ -19,10 +19,11 @@ namespace Phoneword
 
         public override void ViewDidLoad()
         {
-            neededKpi = new List<Kpi>();
+            neededKpi = new List<Kpi>(); //Used as validation for HTTP request
             //string BASE_URL = "http://virtualdealershipadvisorapi.azurewebsites.net/api/";
-            string BASE_URL = "http://msufall2017virtualdealershipadviserapi.azurewebsites.net/api/"; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<New Post
-            //Team database containing info on each dealer
+            string BASE_URL = "http://msufall2017virtualdealershipadviserapi.azurewebsites.net/api/"; 
+            //New azure database website, information on each dealer
+
 
             string url, json_string, dealer_name, query;
 
@@ -32,33 +33,29 @@ namespace Phoneword
             client = new HttpClient();
 
             client.DefaultRequestHeaders.Accept.Clear();
-            //add any default headers below this
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
 
 
-
-            //var viewController = segue.DestinationViewController as ViewController;
-
             ViewController viewcontroller = Storyboard.InstantiateViewController("ViewController") as ViewController;
+            //Determine next set of view to segue to
+
             LoginButton.TouchUpInside += (object sender, EventArgs e) =>
-            {//code for transitioning from login view to home view when login button is clicked
+            {//Code for transitioning from login view to home view when login button is clicked
                
 
                 if (viewcontroller != null)
-                {
+                { //If the view controller is null for somereason, does not transfer 
+
                     dealer_name = UsernameTextfield.Text;
                     url = $"{BASE_URL}NeededKpi?dealer_name={dealer_name}";
                     response = client.GetAsync(url).Result;
 
-                    //json_string = response.Content.ReadAsStringAsync().Result;
-                    //neededKpi = JsonConvert.DeserializeObject<List<Kpi>>(json_string);
-
                     if (response.StatusCode != System.Net.HttpStatusCode.OK)
                     {
-                        new UIAlertView("GET /NeededKpi ERR:", response.StatusCode.ToString(), null, "OK", null).Show();
-
-                        neededKpi.Add(new Kpi { name = "Error finding your needed KPIs" });
+                        //new UIAlertView("GET /NeededKpi ERR:", response.StatusCode.ToString(), null, "OK", null).Show();
+                        new UIAlertView("Bad Response Recieved", "The Database is currently unavailable, please try again another time", null, "OK", null).Show();
+                        //Error code message
                     }
                     else
                     {
@@ -67,24 +64,22 @@ namespace Phoneword
 
                         if (neededKpi == null)
                         {
-                            new UIAlertView("Deserialization ERR", $"JSON Returned: \"{json_string}\"", null, "OK", null).Show();
-                            neededKpi.Add(new Kpi { name = "Error deserializing your needed kpis" });
+                            //new UIAlertView("Deserialization ERR", $"JSON Returned: \"{json_string}\"", null, "OK", null).Show();
+                            new UIAlertView("Invalid Credentials","Invalid Username or Password, please enter valid credentials",null,"OK",null).Show();
+                            //Invalid credentials error message
+
+                            //This is never reached because if the needed KPI is null, then response.StatusCode will not equal ok, I presume
                         }
 
                         else
                         {
-                            new UIAlertView("Welcome: " + dealer_name + " I am ViDA your Virtual Dealership Adviser", null, null, "OK", null).Show();
-                            viewcontroller.neededKpi = neededKpi; //Pass needed KPI to the next view (essentially the dealer's data
-                            viewcontroller.dealer_name = dealer_name;
-                            //PasswordTextfield.Text = neededKpi[0].name + "," + neededKpi[0].p_val.ToString();
+                            new UIAlertView("Welcome" + dealer_name," I am your Virtual Dealership Adviser, ready to help", null, "OK", null).Show();
+                            viewcontroller.neededKpi = neededKpi; //Pass needed KPI to the next view (essentially the dealer's data)
+                            viewcontroller.dealer_name = dealer_name; //Pass dealer's name to next view
 
                             this.NavigationController.PushViewController(viewcontroller, true);
                         }
                     }
-
-
-
-
                 }
             };
         }
